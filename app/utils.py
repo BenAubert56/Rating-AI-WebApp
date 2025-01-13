bfddc3de0dde2f9e569c25ddd289d36320ed8dd4
kylian <kykylh72@gmail.com>
2025-01-13 16:05:44 +0100

    Remove debug print statements from predict_comment_rating
    Eliminated unnecessary print statements used for debugging in the `predict_comment_rating` function. This improves code cleanliness and prevents unintended console outputs during function execution.

--- app/utils.py
+++ app/utils.py
@@ -7,12 +7,8 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
 def predict_comment_rating(comment):
-    print("----------------------------------------------------")
-    print(comment)
     inputs = tokenizer(comment, return_tensors="pt", truncation=True, padding=True, max_length=512)
     outputs = model(**inputs)
     scores = outputs.logits
-    print(scores)
     predicted_rating = torch.argmax(scores, dim=1).item() + 1  # Ajouter +1 car les notes sont indexées de 0 à 4
-    print(predicted_rating)
     return predicted_rating
